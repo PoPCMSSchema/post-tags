9a6c8f8e818ec6d344659a6f5f6134b1957bc283
Leonardo Losoviz <leo@getpop.org>
2020-10-14 13:24:30 +0800

    Use autowiring

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace PoPSchema\PostTags\Config;
 
-use PoP\ComponentModel\Container\ContainerBuilderUtils;
 use PoP\Root\Component\PHPServiceConfigurationTrait;
+use PoP\ComponentModel\Container\ContainerBuilderUtils;
+use PoP\ModuleRouting\RouteModuleProcessorManagerInterface;
 
 class ServiceConfiguration
 {
@@ -17,14 +18,14 @@ class ServiceConfiguration
         // Load API and RESTAPI conditional classes
         if (class_exists('\PoP\API\Component') && \PoP\API\Component::isEnabled()) {
             ContainerBuilderUtils::injectServicesIntoService(
-                'route_module_processor_manager',
+                RouteModuleProcessorManagerInterface::class,
                 'PoPSchema\\PostTags\\Conditional\\API\\RouteModuleProcessors',
                 'add'
             );
         }
         if (class_exists('\PoP\RESTAPI\Component') && \PoP\RESTAPI\Component::isEnabled()) {
             ContainerBuilderUtils::injectServicesIntoService(
-                'route_module_processor_manager',
+                RouteModuleProcessorManagerInterface::class,
                 'PoPSchema\\PostTags\\Conditional\\RESTAPI\\RouteModuleProcessors',
                 'add'
             );
